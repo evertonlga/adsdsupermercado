687696c5d5e3d21d153fbc1fb8e59fa9684afc7e
leonardo.cid@gmail.com <leonardo.cid@gmail.com@1712f06c-9960-11dd-a56d-472d35d90729>
2008-10-15 10:20:58 +0000

    [Leo]: adicionando as métricas para o caixa.

--- trunk/ADSD/src/Caixa.java
+++ trunk/ADSD/src/Caixa.java
@@ -1,12 +1,14 @@
 import eduni.simjava.Sim_entity;
 import eduni.simjava.Sim_event;
 import eduni.simjava.Sim_port;
+import eduni.simjava.Sim_stat;
 import eduni.simjava.Sim_system;
 import eduni.simjava.distributions.Sim_normal_obj;
 
 
 public class Caixa extends Sim_entity{
 	
+	private Sim_stat stat;
 	private Sim_port chegada;
 	private Sim_normal_obj delay;	
 	private double q;
@@ -21,6 +23,13 @@ public class Caixa extends Sim_entity{
       this.q = q;
       chegada = new Sim_port("chegadaCaixa");
       add_port(chegada);
+      stat = new Sim_stat();
+      stat.add_measure(Sim_stat.UTILISATION); //taxa de utilização
+	  stat.add_measure(Sim_stat.SERVICE_TIME); //tempo de serviço
+	  stat.add_measure(Sim_stat.WAITING_TIME); //tempo de espera
+	  stat.measure_for(new int[] { 0, 1 } );
+		
+	  set_stat(stat);
     }
 	
 	public int getVendasEfetuadas(){
